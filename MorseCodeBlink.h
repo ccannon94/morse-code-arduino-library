06fd4e591f9a7c5d1ca48cd8cd5fb65d63bfccbb
Chris Cannon <chris.c.cannon@gmail.com>
2016-08-28 17:22:49 -0400

    Create file header/copyright information
    Relates to: #1

---
+++ MorseCodeBlink.h
@@ -0,0 +1,11 @@
+/**
+    Morse Code Blink library for Arduino Uno
+
+    This library contains LED blink sequences for Morse Code on Arduino boards.
+    By default, this library works with pin 13 of an Arduino Uno.
+
+    Published under GNU General Public License v3
+
+    @author Chris Cannon
+    @version 0.1 08/28/2016
+*/
