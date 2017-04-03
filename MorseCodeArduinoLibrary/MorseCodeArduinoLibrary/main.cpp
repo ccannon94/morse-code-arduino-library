2ff8cb84140d847c4b0ba339ab0396ea4b924bd4
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-03 15:36:03 -0400

    Include library in xCode project for testing purposes

---
+++ MorseCodeArduinoLibrary/MorseCodeArduinoLibrary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MorseCodeArduinoLibrary
+//
+//  Created by Christopher Cannon on 03/Apr/17.
+//  Copyright © 2017 c3_development. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
